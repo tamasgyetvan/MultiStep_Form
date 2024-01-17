a9e55fcb22af99b076fe54991c6bff6241fd4d76
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-01-17 13:51:48 +0100

    Create ButtonBar component

---
+++ src/components/ButtonBar.tsx
@@ -0,0 +1,32 @@
+type ButtonBarProps = {
+  currentStep: number;
+  incrementStep: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  decrementStep: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+export const ButtonBar = ({
+  currentStep,
+  incrementStep,
+  decrementStep,
+}: ButtonBarProps) => {
+  return (
+    <section className="buttonBar">
+      <>
+        {currentStep !== 1 ? (
+          <button className="backButton" onClick={decrementStep}>
+            Go back
+          </button>
+        ) : null}
+        {currentStep !== 4 ? (
+          <button className="nextButton" onClick={incrementStep}>
+            Next step
+          </button>
+        ) : (
+          <button className="confirmButton" onClick={() => {}}>
+            Confirm
+          </button>
+        )}
+      </>
+    </section>
+  );
+};
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,28 @@
 import { ProgressBar } from "./ProgressBar";
-
+import { ButtonBar } from "./ButtonBar";
+import { useState } from "react";
 export const Form = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  function incrementStep(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setCurrentStep(currentStep + 1);
+  }
+
+  function decrementStep(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setCurrentStep(currentStep - 1);
+  }
   return (
     <div className="formContainer">
       <ProgressBar />
-      <div></div>
+      <form>
+        <ButtonBar
+          currentStep={currentStep}
+          incrementStep={incrementStep}
+          decrementStep={decrementStep}
+        />
+      </form>
     </div>
   );
 };
