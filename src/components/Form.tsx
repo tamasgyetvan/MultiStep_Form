11f677439b4b2a10f50603749c87a91bb0d2c33f
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-01-30 09:24:53 +0100

    Implement react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,29 +2,82 @@ import { ProgressBar } from "./ProgressBar";
 import { ButtonBar } from "./ButtonBar";
 import { useState } from "react";
 import { PersonalInfoForm } from "./PersonalInfoForm";
+import { useForm } from "react-hook-form";
 export const Form = () => {
   const [currentStep, setCurrentStep] = useState(1);
 
-  function incrementStep(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    setCurrentStep(currentStep + 1);
-  }
-
-  function decrementStep(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    setCurrentStep(currentStep - 1);
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="multiStepForm">
       <ProgressBar currentStep={currentStep} />
-      <div className="formContainer">
-        <PersonalInfoForm />
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+        className="personalInfoForm"
+      >
+        <h1 className="formTitle">Personal info</h1>
+        <p className="formDescription">
+          Please provide your name, email address, and phone number.
+        </p>
+        <label>
+          {errors.name && <p>{`${errors.name.message}`}</p>}
+          Name
+          <input
+            {...register("name", {
+              required: "This field is required",
+              minLength: {
+                value: 3,
+                message: "Minimum 3",
+              },
+            })}
+            type="text"
+            name="name"
+          />
+        </label>
+        <label>
+          {errors.email && <p>{`${errors.email.message}`}</p>}
+          Email
+          <input
+            {...register("email", {
+              required: "This field is required",
+              minLength: {
+                value: 3,
+                message: "Minimum 3",
+              },
+            })}
+            type="text"
+            name="email"
+          />
+        </label>
+        <label>
+          {errors.phone && <p>{`${errors.phone.message}`}</p>}
+          Phone
+          <input
+            {...register("phone", {
+              required: "This field is required",
+              minLength: {
+                value: 3,
+                message: "Minimum 3",
+              },
+            })}
+            type="text"
+            name="phone"
+          />
+        </label>
+
         <ButtonBar
           currentStep={currentStep}
-          incrementStep={incrementStep}
-          decrementStep={decrementStep}
+          incrementStep={() => {}}
+          decrementStep={() => {
+            setCurrentStep(currentStep - 1);
+          }}
         />
-      </div>
+      </form>
     </div>
   );
 };
